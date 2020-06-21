0a6b9f07de44c1f11aaa177abe0a582fbd622866
David <daveshuang@gmail.com>
2020-06-21 13:42:17 -0700

    fixed

--- interviewQuestions/easy/arrays/intersectionTwoArr.js
+++ interviewQuestions/easy/arrays/intersectionTwoArr.js
@@ -1,13 +1,17 @@
 const intersect = (nums1, nums2) => {
-    const set = new Set();
+    const map = new Map();
     const result = [];
     for (let i = 0; i < nums1.length; i++) {
-        set.add(nums1[i]);
+        if (map.has(nums1[i])) {
+            map.set(nums1[i], map.get(nums1[i]) + 1);
+        } else {
+            map.set(nums1[i], 1);
+        }
     }
     for (let i = 0; i < nums2.length; i++) {
-        if (set.has(nums2[i])) {
+        if (map.has(nums2[i]) && map.get(nums2[i]) > 0) {
             result.push(nums2[i]);
-            //set.delete(nums2[i]);
+            map.set(nums2[i], map.get(nums2[i]) - 1);
         }
     }
     return result;
