31cc077be27b2c40255b0a9732c30c2d87a8bbd7
David <daveshuang@gmail.com>
2020-06-22 14:45:21 -0700

    plus one

---
+++ interviewQuestions/easy/arrays/plusOne.js
@@ -0,0 +1,16 @@
+const plusOne = digits => {
+    let temp = 0;
+    digits[digits.length - 1]++;
+    for (let i = digits.length - 1; i >= 0; i--) {
+        if (digits[i] >= 10) {
+            temp = (digits[i] / 10);
+            digits[i] = digits[i] % 10;
+            if (i === 0) {
+                digits.unshift(temp);
+            } else {
+                digits[i - 1] += temp;
+            }
+        }
+    }
+    return digits;
+};
