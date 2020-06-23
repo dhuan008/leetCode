3a3d7a1d5d517a38da922cfca32fa1effcfa1f33
David <daveshuang@gmail.com>
2020-06-23 12:16:39 -0700

    revised solution

--- interviewQuestions/easy/arrays/plusOne.js
+++ interviewQuestions/easy/arrays/plusOne.js
@@ -14,3 +14,22 @@ const plusOne = digits => {
     }
     return digits;
 };
+
+// revised
+const plusOne = digits => {
+    let temp = 0;
+    digits[digits.length - 1]++;
+    for (let i = digits.length - 1; i >= 0; i--) {
+        if (digits[i] < 10) {
+            return digits;
+        }
+        temp = (digits[i] / 10);
+        digits[i] = digits[i] % 10;
+        if (i === 0) {
+            digits.unshift(temp);
+        } else {
+            digits[i - 1] += temp;
+        }
+    }
+    return digits;
+};
