b332b85fbd873bb0b8fb2bad86c8b100a546dc16
David <daveshuang@gmail.com>
2020-06-27 14:50:28 -0700

    added 32 bit overflow testing

--- interviewQuestions/easy/Strings/reverseInt.js
+++ interviewQuestions/easy/Strings/reverseInt.js
@@ -12,3 +12,29 @@ const reverse = x => {
     }
     return reversedInt < Math.pow(-2, 31) || reversedInt > Math.pow(2, 31) ? 0 : reversedInt;
 };
+
+/**
+ * @param {number} x
+ * @return {number}
+ * Overflow testing if 32 bit
+ */
+const reverse = x => {
+    let reversedInt = 0,
+        pop = 0,
+        pos = true;
+    if (x < 0) {
+        pos = false;
+    }
+    while (x) {
+        pop = x % 10;
+        x = parseInt(x/10);
+        reversedInt = reversedInt * 10 + pop;
+        if (reversedInt < 0 && pos) {
+            return 0;
+        }
+        if (reversedInt >= 0 && !pos) {
+            return 0;
+        }
+    }
+    return reversedInt;
+};
