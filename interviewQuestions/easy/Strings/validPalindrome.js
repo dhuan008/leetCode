b52615df1f46c54ca2f0c6cc846ff95d94a2d726
David <daveshuang@gmail.com>
2020-06-30 15:32:59 -0700

    valid palindrome

---
+++ interviewQuestions/easy/Strings/validPalindrome.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+const isPalindrome = s => {
+    if (s.length <= 1) {
+        return true;
+    }
+    s = s.replace(/[^a-zA-Z0-9]/g,"").toLowerCase();
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] !== s[s.length - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
+};
