625dd7b04f6d13af1b1f0d1f90519dfc946f8c65
David <daveshuang@gmail.com>
2020-08-09 21:35:19 -0700

    number of 1 bits

---
+++ interviewQuestions/easy/others/numOf1Bits.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} n - a positive integer
+ * @return {number}
+ */
+const hammingWeight = n => {
+    let counter = 0;
+    for (let i = 0; i < 32; i++) {
+        if (n & 1 === 1) {
+            counter++;
+        }
+        n >>= 1;
+    }
+    return counter;
+};
