9ff4653f7ecdb96b8552e83cabb7c39739a1a1ef
David <daveshuang@gmail.com>
2020-08-22 14:37:19 -0700

    WIP - 3sum

---
+++ interviewQuestions/medium/arrayAndStrings/3sum.js
@@ -0,0 +1,10 @@
+const threeSum = (nums) => {
+  nums.sort((a, b) => a - b);
+  const result = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    let low = i + 1,
+      high = nums.length - 1,
+      sum = 0;
+  }
+};
